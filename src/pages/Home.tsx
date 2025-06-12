896883478205a815ac9d32e886749eccf5982e0f
Artem Petrovskiy <artvpart@gmail.com>
2025-06-12 18:45:42 +0300

    add fetch data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState({
+    title: "Loading...",
+    description: "Loading...",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("content/hero.json");
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="container py-10 text-center">
-      <h1 className="mb-5 text-5xl font-bold">Welcome to my React app</h1>
-      <div>This is a simple React app using Vite and Tailwind CSS.</div>
+      <h1 className="mb-5 text-5xl font-bold">{data.title}</h1>
+      <div>{data.description}</div>
     </section>
   );
 }
